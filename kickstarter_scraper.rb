1ad019981a6a41e34c7f214c7d60cc72504df7c6
Will Lee <will.i.lee@outlook.com>
2018-07-16 19:59:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,13 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  projects = {}
+
+  #Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 
 end
 create_project_hash
