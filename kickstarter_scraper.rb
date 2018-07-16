4f3cc43e791b45a579f5de75dcd25bb2782c7509
Will Lee <will.i.lee@outlook.com>
2018-07-16 20:03:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 
   # projects: kickstarter.css("li.project.grid_4")
   # titles: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # image_link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name)".text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
@@ -21,7 +21,12 @@ def create_project_hash
   #Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      description => project.css("p.bbcard_blurb").text,
+      location => project.css("span.location-name)".text,
+      percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
   end
 
   #return projects hash
