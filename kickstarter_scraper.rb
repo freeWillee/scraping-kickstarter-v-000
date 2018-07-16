81d5508817a4c449cad9a86ab46093ed9e773a66
Will Lee <will.i.lee@outlook.com>
2018-07-16 19:57:37 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,13 @@
 require 'nokogiri'
 require 'pry'
 
+  # projects: kickstarter.css("li.project.grid_4")
+  # titles: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("span.location-name)".text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
 def create_project_hash
   # write your code here
   #This just opens a file and reads it into a variable
@@ -10,11 +17,5 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 
-  # projects: kickstarter.css("li.project.grid_4")
-  # titles: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("span.location-name)".text
-  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 end
 create_project_hash
