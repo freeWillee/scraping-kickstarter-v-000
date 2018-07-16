61a27e8d9af2493252bbb20a9fa27203ccc482e6
Will Lee <will.i.lee@outlook.com>
2018-07-16 19:53:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,5 +13,6 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   # titles: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
 end
 create_project_hash
